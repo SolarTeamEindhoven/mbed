8cd40698673bce71201282a2af06726660af2372
Dirk van Wijk <developer@dirkwillem.nl>
2019-07-10 11:23:44 +0200

    Removed unused parameters that resulted in warnings during compilation

--- libraries/mbed/api/FileSystemLike.h
+++ libraries/mbed/api/FileSystemLike.h
@@ -61,7 +61,7 @@ public:
      *  @param filename the name of the file to remove.
      *  @param returns 0 on success, -1 on failure.
      */
-    virtual int remove(const char *filename) { return -1; };
+    virtual int remove(const char*) { return -1; };
 
     /** Rename a file in the filesystem.
      *
@@ -72,7 +72,7 @@ public:
      *    0 on success,
      *   -1 on failure.
      */
-    virtual int rename(const char *oldname, const char *newname) { return -1; };
+    virtual int rename(const char*, const char*) { return -1; };
 
     /** Opens a directory in the filesystem and returns a DirHandle
      *   representing the directory stream.
@@ -83,7 +83,7 @@ public:
      *    A DirHandle representing the directory stream, or
      *   NULL on failure.
      */
-    virtual DirHandle *opendir(const char *name) { return NULL; };
+    virtual DirHandle *opendir(const char*) { return NULL; };
 
     /** Creates a directory in the filesystem.
      *
@@ -94,7 +94,7 @@ public:
      *    0 on success,
      *   -1 on failure.
      */
-    virtual int mkdir(const char *name, mode_t mode) { return -1; }
+    virtual int mkdir(const char*, mode_t) { return -1; }
 
     // TODO other filesystem functions (mkdir, rm, rn, ls etc)
 };
